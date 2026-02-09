9593b72a1f102cd7ac6651fc4eec1c6932f09480
greeenboi <suvan.gowrishanker.204@gmail.com>
2026-02-09 14:53:43 +0530

    feat: add rendering functionality and update dependencies

---
+++ content/render.mjs
@@ -0,0 +1,40 @@
+import {bundle} from '@remotion/bundler';
+import {renderMedia, selectComposition} from '@remotion/renderer';
+import path from 'path';
+ 
+// The composition you want to render
+const compositionId = 'CrdtExplainer';
+ 
+// You only have to create a bundle once, and you may reuse it
+// for multiple renders that you can parametrize using input props.
+const bundleLocation = await bundle({
+  entryPoint: path.resolve('./src/index.ts'),
+  // If you have a webpack override in remotion.config.ts, pass it here as well.
+  webpackOverride: (config) => config,
+});
+ 
+// Parametrize the video by passing props to your component.
+const inputProps = {
+  logLevel: 'verbose',
+  chromiumOptions: "--timeout 60"
+};
+ 
+// Get the composition you want to render. Pass `inputProps` if you
+// want to customize the duration or other metadata.
+const composition = await selectComposition({
+  serveUrl: bundleLocation,
+  id: compositionId,
+  inputProps,
+});
+ 
+// Render the video. Pass the same `inputProps` again
+// if your video is parametrized with data.
+await renderMedia({
+  composition,
+  serveUrl: bundleLocation,
+  codec: 'h264',
+  outputLocation: `out/Carnelia-product-demo.mp4`,
+  inputProps,
+});
+ 
+console.log('Render done!');
